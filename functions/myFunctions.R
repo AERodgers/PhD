4cfca1330e7f2ef37ce3712afe929463b521c525
Antoin Eoin Rodgers <46627448+AERodgers@users.noreply.github.com>
2022-05-13 17:18:26 +0100

    almost weekend

--- functions/myFunctions.R
+++ functions/myFunctions.R
@@ -147,23 +147,24 @@ AdjustPToBonferroni <- function(myTibble,
 }
 
 
-sigCodesTidy <- function(myTibble) {
-  myTibble <- mutate(myTibble,
-                     "signif. (adj.)" =
-                       if_else(
-                         p.adjusted < 0.001,
-                         'p<0.001',
-                         if_else(
-                           p.adjusted < 0.01,
-                           'p<0.01',
-                           if_else(
-                             p.adjusted < 0.05,
-                             'p<0.05',
-                             if_else(p.adjusted < 0.1,
-                                     '(p<0.1)',
-                                     '')
-                           )
-                         )
-                       ))
-  return(myTibble)
+sigCodesTidy <- function(my_tibble, incl_marginal_sig = FALSE) {
+  my_tibble <- mutate(my_tibble,
+                      "signif. (adj.)" =
+                        if_else(
+                          p.adjusted < 0.001,
+                          'p<0.001',
+                          if_else(
+                            p.adjusted < 0.01,
+                            'p<0.01',
+                            if_else(
+                              p.adjusted < 0.05,
+                              'p<0.05',
+                              if_else(p.adjusted < 0.1 &
+                                        incl_marginal_sig,
+                                      '(p<0.1)',
+                                      '')
+                            )
+                          )
+                        ))
+  return(my_tibble)
 }
