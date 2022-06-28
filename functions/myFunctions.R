d172174e1ae800486cc54cfdf96923d7c269d841
Antoin Eoin Rodgers <46627448+AERodgers@users.noreply.github.com>
2022-06-28 18:28:15 +0100

    end of day
    working on excel spreadsheets looking right

--- functions/myFunctions.R
+++ functions/myFunctions.R
@@ -795,6 +795,30 @@ tidyPairwise <- function(all_models_tidy)
   )
 }
 
+################################################################################
+kable_chi_sq <- function(chi_sq_test)
+    {
+    # returns a kable() object of the chi_sq_test input.
+    require(knitr,)
+    require(janitor)
+
+    x2d <- round(chi_sq_test$statistic[1],10)
+    names(x2d) <- NULL
+
+    df <- chi_sq_test$parameter[1]
+    names(df) <- NULL
+
+    p <-chi_sq_test$p.value[1]
+
+
+    df <- data.frame(term=c("Chi-squared", "df", "p.value"),
+                    value=c(x2d, df, p))
+
+    names(df) <- NULL
+    return(kable(df, caption="Pearson's Chi-squared test"))
+
+
+}
 
 
 
