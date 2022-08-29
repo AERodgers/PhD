84058a3922300c82c120942b1733a9027deb59fc
AERodgers <rodgeran@tcd.ie>
2022-08-29 02:35:24 +0100

    Reverted to by-subcorpus analysis
    Why did I ever decide NOT to do this?
    The corpora are smaller and not perfect, but by using all the data, which were designed for other purposes, there is far too much skewing of the analysis. For example, 55 examples of ana_2 and ana_3, but hundreds for ana_1 and ana_0...

--- Ch_6_Form/z_archive/old/old_root/Output_PN_anac.R
+++ Ch_6_Form/z_archive/old/old_root/Output_PN_anac.R
@@ -12,14 +12,14 @@ i = 1
 
   PN.anac.model.L_t.sum = summary(PN.anac.model.L_t)
   PN.anac.model.L_t.ano = anova(PN.anac.model.L_t)
-  fileName = paste("PN_anac_modelOutput/1_PN.anac.model.L_t_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_anac_model6_1_output/1_PN.anac.model.L_t_", syls[i], "syl.txt", sep = "")
   capture.output(PN.anac.model.L_t.sum, file = fileName)
   capture.output(PN.anac.model.L_t.ano, file = fileName, append =  TRUE)
 
   tab_model(PN.anac.model.L_t,
             title = paste("L target time (ms),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_anac_modelOutput/1_PN.L_t.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_anac_model6_1_output/1_PN.L_t.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.anac.model.L_t,
              title = paste("L target time (ms),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -27,21 +27,21 @@ i = 1
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_anac_modelOutput/1_PN.L_t.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_anac_model6_1_output/1_PN.L_t.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
 
 
 
   PN.anac.model.H_t.sum = summary(PN.anac.model.H_t)
   PN.anac.model.H_t.ano = anova(PN.anac.model.H_t)
-  fileName = paste("PN_anac_modelOutput/2_PN.anac.model.H_t_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_anac_model6_1_output/2_PN.anac.model.H_t_", syls[i], "syl.txt", sep = "")
   capture.output(PN.anac.model.H_t.sum, file = fileName)
   capture.output(PN.anac.model.H_t.ano, file = fileName, append =  TRUE)
 
   tab_model(PN.anac.model.H_t,
             title = paste("H target time (ms),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_anac_modelOutput/2_PN.H_t.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_anac_model6_1_output/2_PN.H_t.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.anac.model.H_t,
              title = paste("H target time (ms),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -49,21 +49,21 @@ i = 1
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_anac_modelOutput/2_PN.H_t.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_anac_model6_1_output/2_PN.H_t.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
 
 
 
   PN.anac.model.L_fo.sum = summary(PN.anac.model.L_fo)
   PN.anac.model.L_fo.ano = anova(PN.anac.model.L_fo)
-  fileName = paste("PN_anac_modelOutput/3_PN.anac.model.L_fo_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_anac_model6_1_output/3_PN.anac.model.L_fo_", syls[i], "syl.txt", sep = "")
   capture.output(PN.anac.model.L_fo.sum, file = fileName)
   capture.output(PN.anac.model.L_fo.ano, file = fileName, append =  TRUE)
 
   tab_model(PN.anac.model.L_fo,
             title = paste("L target fo (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_anac_modelOutput/3_PN.L_fo.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_anac_model6_1_output/3_PN.L_fo.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.anac.model.L_fo,
              title = paste("L target fo (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -71,20 +71,20 @@ i = 1
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_anac_modelOutput/3_PN.L_fo.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_anac_model6_1_output/3_PN.L_fo.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
 
 
   PN.anac.model.H_fo.sum = summary(PN.anac.model.H_fo)
   PN.anac.model.H_fo.ano = anova(PN.anac.model.H_fo)
-  fileName = paste("PN_anac_modelOutput/4_PN.anac.model.H_fo_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_anac_model6_1_output/4_PN.anac.model.H_fo_", syls[i], "syl.txt", sep = "")
   capture.output(PN.anac.model.H_fo.sum, file = fileName)
   capture.output(PN.anac.model.H_fo.ano, file = fileName, append =  TRUE)
 
   tab_model(PN.anac.model.H_fo,
             title = paste("H target fo (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_anac_modelOutput/4_PN.H_fo.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_anac_model6_1_output/4_PN.H_fo.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.anac.model.H_fo,
              title = paste("H target fo (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -92,14 +92,14 @@ i = 1
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_anac_modelOutput/4_PN.H_fo.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_anac_model6_1_output/4_PN.H_fo.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
 
 
 
   PN.anac.model.med.sum = summary(PN.anac.model.med)
   PN.anac.model.med.ano = anova(PN.anac.model.med)
-  fileName = paste("PN_anac_modelOutput/5_PN.anac.model.med_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_anac_model6_1_output/5_PN.anac.model.med_", syls[i], "syl.txt", sep = "")
   capture.output(PN.anac.model.med.sum, file = fileName)
   capture.output(PN.anac.model.med.ano, file = fileName, append =  TRUE)
 
@@ -107,7 +107,7 @@ i = 1
   tab_model(PN.anac.model.med,
             title = paste("Median fo (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_anac_modelOutput/5_PN.med.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_anac_model6_1_output/5_PN.med.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.anac.model.med,
              title = paste("Median fo (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -115,20 +115,20 @@ i = 1
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_anac_modelOutput/5_PN.med.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_anac_model6_1_output/5_PN.med.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
 
 
   PN.anac.model.mean.sum = summary(PN.anac.model.mean)
   PN.anac.model.mean.ano = anova(PN.anac.model.mean)
-  fileName = paste("PN_anac_modelOutput/6_PN.anac.model.mean_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_anac_model6_1_output/6_PN.anac.model.mean_", syls[i], "syl.txt", sep = "")
   capture.output(PN.anac.model.mean.sum, file = fileName)
   capture.output(PN.anac.model.mean.ano, file = fileName, append =  TRUE)
 
   tab_model(PN.anac.model.mean,
             title = paste("Mean fo (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_anac_modelOutput/6_PN.mean.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_anac_model6_1_output/6_PN.mean.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.anac.model.mean,
              title = paste("Mean fo (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -136,20 +136,20 @@ i = 1
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_anac_modelOutput/6_PN.mean.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_anac_model6_1_output/6_PN.mean.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
 
 
   PN.anac.model.lh_slope.sum = summary(PN.anac.model.lh_slope)
   PN.anac.model.lh_slope.ano = anova(PN.anac.model.lh_slope)
-  fileName = paste("PN_anac_modelOutput/7_PN.anac.model.lh_slope_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_anac_model6_1_output/7_PN.anac.model.lh_slope_", syls[i], "syl.txt", sep = "")
   capture.output(PN.anac.model.lh_slope.sum, file = fileName)
   capture.output(PN.anac.model.lh_slope.ano, file = fileName, append =  TRUE)
 
   tab_model(PN.anac.model.lh_slope,
             title = paste("Slope (ST/sec),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_anac_modelOutput/7_PN.lh_slope.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_anac_model6_1_output/7_PN.lh_slope.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.anac.model.lh_slope,
              title = paste("Slope (ST/sec),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -157,21 +157,21 @@ i = 1
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_anac_modelOutput/7_PN.lh_slope.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_anac_model6_1_output/7_PN.lh_slope.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
 
 
 
   PN.anac.model.R_fo.sum = summary(PN.anac.model.R_fo)
   PN.anac.model.R_fo.ano = anova(PN.anac.model.R_fo)
-  fileName = paste("PN_anac_modelOutput/7_PN.anac.model.R_fo_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_anac_model6_1_output/7_PN.anac.model.R_fo_", syls[i], "syl.txt", sep = "")
   capture.output(PN.anac.model.R_fo.sum, file = fileName)
   capture.output(PN.anac.model.R_fo.ano, file = fileName, append =  TRUE)
 
   tab_model(PN.anac.model.R_fo,
             title = paste("Range (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_anac_modelOutput/8_PN.R_fo.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_anac_model6_1_output/8_PN.R_fo.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.anac.model.R_fo,
              title = paste("Range (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -179,5 +179,5 @@ i = 1
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_anac_modelOutput/8_PN.R_fo.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_anac_model6_1_output/8_PN.R_fo.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
--- Ch_6_Form/z_archive/old/old_root/Output_PN_foot.R
+++ Ch_6_Form/z_archive/old/old_root/Output_PN_foot.R
@@ -13,14 +13,14 @@ i = 4
 
   PN.ft.model.L_t.sum = summary(PN.ft.model.L_t)
   PN.ft.model.L_t.ano = anova(PN.ft.model.L_t)
-  fileName = paste("PN_foot_modelOutput/1_PN.ft.model.L_t_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/1_PN.ft.model.L_t_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.L_t.sum, file = fileName)
   capture.output(PN.ft.model.L_t.ano, file = fileName, append =  TRUE)
   
   tab_model(PN.ft.model.L_t,
             title = paste("L target time (ms),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/1_PN.L_t.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/1_PN.L_t.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.L_t,
              title = paste("L target time (ms),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -28,21 +28,21 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/1_PN.L_t.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/1_PN.L_t.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
   
   
   
   PN.ft.model.H_t.sum = summary(PN.ft.model.H_t)
   PN.ft.model.H_t.ano = anova(PN.ft.model.H_t)
-  fileName = paste("PN_foot_modelOutput/2_PN.ft.model.H_t_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/2_PN.ft.model.H_t_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.H_t.sum, file = fileName)
   capture.output(PN.ft.model.H_t.ano, file = fileName, append =  TRUE)
   
   tab_model(PN.ft.model.H_t,
             title = paste("H target time (ms),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/2_PN.H_t.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/2_PN.H_t.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.H_t,
              title = paste("H target time (ms),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -50,21 +50,21 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/2_PN.H_t.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/2_PN.H_t.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
   
   
   
   PN.ft.model.L_fo.sum = summary(PN.ft.model.L_fo)
   PN.ft.model.L_fo.ano = anova(PN.ft.model.L_fo)
-  fileName = paste("PN_foot_modelOutput/3_PN.ft.model.L_fo_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/3_PN.ft.model.L_fo_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.L_fo.sum, file = fileName)
   capture.output(PN.ft.model.L_fo.ano, file = fileName, append =  TRUE)
   
   tab_model(PN.ft.model.L_fo,
             title = paste("L target fo (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/3_PN.L_fo.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/3_PN.L_fo.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.L_fo,
              title = paste("L target fo (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -72,20 +72,20 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/3_PN.L_fo.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/3_PN.L_fo.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
   
   
   PN.ft.model.H_fo.sum = summary(PN.ft.model.H_fo)
   PN.ft.model.H_fo.ano = anova(PN.ft.model.H_fo)
-  fileName = paste("PN_foot_modelOutput/4_PN.ft.model.H_fo_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/4_PN.ft.model.H_fo_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.H_fo.sum, file = fileName)
   capture.output(PN.ft.model.H_fo.ano, file = fileName, append =  TRUE)
   
   tab_model(PN.ft.model.H_fo,
             title = paste("H target fo (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/4_PN.H_fo.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/4_PN.H_fo.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.H_fo,
              title = paste("H target fo (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -93,14 +93,14 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/4_PN.H_fo.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/4_PN.H_fo.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
   
   
   
   PN.ft.model.med.sum = summary(PN.ft.model.med)
   PN.ft.model.med.ano = anova(PN.ft.model.med)
-  fileName = paste("PN_foot_modelOutput/5_PN.ft.model.med_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/5_PN.ft.model.med_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.med.sum, file = fileName)
   capture.output(PN.ft.model.med.ano, file = fileName, append =  TRUE)
   
@@ -108,7 +108,7 @@ i = 4
   tab_model(PN.ft.model.med,
             title = paste("Median fo (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/5_PN.med.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/5_PN.med.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.med,
              title = paste("Median fo (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -116,20 +116,20 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/5_PN.med.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/5_PN.med.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
   
   
   PN.ft.model.mean.sum = summary(PN.ft.model.mean)
   PN.ft.model.mean.ano = anova(PN.ft.model.mean)
-  fileName = paste("PN_foot_modelOutput/6_PN.ft.model.mean_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/6_PN.ft.model.mean_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.mean.sum, file = fileName)
   capture.output(PN.ft.model.mean.ano, file = fileName, append =  TRUE)
   
   tab_model(PN.ft.model.mean,
             title = paste("Mean fo (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/6_PN.mean.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/6_PN.mean.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.mean,
              title = paste("Mean fo (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -137,20 +137,20 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/6_PN.mean.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/6_PN.mean.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
   
   
   PN.ft.model.lh_slope.sum = summary(PN.ft.model.lh_slope)
   PN.ft.model.lh_slope.ano = anova(PN.ft.model.lh_slope)
-  fileName = paste("PN_foot_modelOutput/7_PN.ft.model.lh_slope_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/7_PN.ft.model.lh_slope_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.lh_slope.sum, file = fileName)
   capture.output(PN.ft.model.lh_slope.ano, file = fileName, append =  TRUE)
   
   tab_model(PN.ft.model.lh_slope,
             title = paste("Slope (ST/sec),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/7_PN.lh_slope.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/7_PN.lh_slope.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.lh_slope,
              title = paste("Slope (ST/sec),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -158,21 +158,21 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/7_PN.lh_slope.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/7_PN.lh_slope.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
 
   
   
   PN.ft.model.R_fo.sum = summary(PN.ft.model.R_fo)
   PN.ft.model.R_fo.ano = anova(PN.ft.model.R_fo)
-  fileName = paste("PN_foot_modelOutput/8_PN.ft.model.R_fo_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/8_PN.ft.model.R_fo_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.R_fo.sum, file = fileName)
   capture.output(PN.ft.model.R_fo.ano, file = fileName, append =  TRUE)
   
   tab_model(PN.ft.model.R_fo,
             title = paste("Range (ST),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/8_PN.R_fo.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/8_PN.R_fo.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.R_fo,
              title = paste("Range (ST),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -180,21 +180,21 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/8_PN.R_fo.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/8_PN.R_fo.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
   
   
   
   PN.ft.model.D_t.sum = summary(PN.ft.model.D_t)
   PN.ft.model.D_t.ano = anova(PN.ft.model.D_t)
-  fileName = paste("PN_foot_modelOutput/9_PN.ft.model.D_t_", syls[i], "syl.txt", sep = "")
+  fileName = paste("PN_foot_model6_1_output/9_PN.ft.model.D_t_", syls[i], "syl.txt", sep = "")
   capture.output(PN.ft.model.D_t.sum, file = fileName)
   capture.output(PN.ft.model.D_t.ano, file = fileName, append =  TRUE)
   
   tab_model(PN.ft.model.D_t,
             title = paste("L to H time (ms),", syls[i], "as intercept"),
             show.icc = FALSE,
-            file = paste("PN_foot_modelOutput/9_PN.D_t.regression_", syls[i], "syl.html", sep = ""))
+            file = paste("PN_foot_model6_1_output/9_PN.D_t.regression_", syls[i], "syl.html", sep = ""))
   plot_model(PN.ft.model.D_t,
              title = paste("L to H time (ms),", syls[i], "as intercept"),
              show.intercept = FALSE,
@@ -202,7 +202,7 @@ i = 4
              #order.terms = c(2,3,4),
              vline.color = "red",
              colors = "Black")
-  ggsave(file = paste("PN_foot_modelOutput/9_PN.D_t.regression.plot_", syls[i], ".png", sep = ""),
+  ggsave(file = paste("PN_foot_model6_1_output/9_PN.D_t.regression.plot_", syls[i], ".png", sep = ""),
          width = 15, height = 8, units = "cm")
   
   
